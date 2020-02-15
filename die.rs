1bc1346d37b3e4b4620b08a83234d3f166dad632
Andrew Palmer <atp@sdf.org>
2020-02-15 09:30:19 -0600

    Correctly implement From trait for Die

--- die.rs
+++ die.rs
@@ -4,8 +4,8 @@ pub struct Die {
     value: i32,
 }
 
-impl Die {
-    pub fn from(value: i32) -> Die {
+impl From<i32> for Die {
+    fn from(value: i32) -> Die {
         assert!(value >=1 && value <= 6, "Die value must be 1-6");
         return Die { value: value };
     }
