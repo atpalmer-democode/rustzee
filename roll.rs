831773ec15e9419ba9d22cad69783da1910c0b7a
Andrew Palmer <atp@sdf.org>
2020-02-15 10:17:48 -0600

    Implement From<[i32; 5]> trait for Roll

--- roll.rs
+++ roll.rs
@@ -7,17 +7,22 @@ pub struct Roll {
     dice: [Die; 5],
 }
 
+impl From<[i32; 5]> for Roll {
+    fn from(value: [i32; 5]) -> Roll {
+        let dice: [Die; 5] = [
+            value[0].into(),
+            value[1].into(),
+            value[2].into(),
+            value[3].into(),
+            value[4].into(),
+        ];
+        return Roll { dice: dice };
+    }
+}
+
 impl Roll {
     pub fn of(a: i32, b: i32, c: i32, d: i32, e: i32) -> Roll {
-        return Roll {
-            dice: [
-                a.into(),
-                b.into(),
-                c.into(),
-                d.into(),
-                e.into(),
-            ],
-        };
+        return Roll::from([a, b, c, d, e]);
     }
 }
 
