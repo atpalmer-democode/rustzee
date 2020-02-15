cbace43df4b56b740cb1c6b8bebcf49048c8af2c
Andrew Palmer <atp@sdf.org>
2020-02-15 11:38:52 -0600

    Keep roll module ignorant of raw i32s

--- roll.rs
+++ roll.rs
@@ -13,19 +13,6 @@ impl From<[Die; 5]> for Roll {
     }
 }
 
-impl From<[i32; 5]> for Roll {
-    fn from(value: [i32; 5]) -> Roll {
-        let dice: [Die; 5] = [
-            value[0].into(),
-            value[1].into(),
-            value[2].into(),
-            value[3].into(),
-            value[4].into(),
-        ];
-        return Roll::from(dice);
-    }
-}
-
 impl Roll {
     pub fn of(a: Die, b: Die, c: Die, d: Die, e: Die) -> Roll {
         return Roll::from([a, b, c, d, e]);
