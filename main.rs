363d8c01722cb40a1ef84280f851966a47dd8e8e
Andrew Palmer <atp@sdf.org>
2020-02-15 09:25:27 -0600

    Move Die struct creation out of main

--- main.rs
+++ main.rs
@@ -1,12 +1,8 @@
-mod die;
-use die::Die;
 mod roll;
 use roll::Roll;
 
 
 fn main() {
     let roll = Roll::from(6, 5, 4, 3, 2);
-    let die = Die::from(6);
     println!("Dice: {}", roll);
-    println!("Die: {}", die);
 }
