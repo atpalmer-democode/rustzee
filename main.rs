d972c9f6e7b59ba460b64a002a1363bd23507f05
Andrew Palmer <atp@sdf.org>
2020-02-16 16:30:42 -0600

    Reorder scoring display

--- main.rs
+++ main.rs
@@ -13,12 +13,12 @@ fn main() {
 
     let reroll = roll.reroll(keep);
     println!("Reroll: {}", reroll);
-    println!("Score as 6s: {}", scoring::score_as(&reroll, 6));
-    println!("Score as 5s: {}", scoring::score_as(&reroll, 5));
-    println!("Score as 4s: {}", scoring::score_as(&reroll, 4));
-    println!("Score as 3s: {}", scoring::score_as(&reroll, 3));
-    println!("Score as 2s: {}", scoring::score_as(&reroll, 2));
     println!("Score as 1s: {}", scoring::score_as(&reroll, 1));
+    println!("Score as 2s: {}", scoring::score_as(&reroll, 2));
+    println!("Score as 3s: {}", scoring::score_as(&reroll, 3));
+    println!("Score as 4s: {}", scoring::score_as(&reroll, 4));
+    println!("Score as 5s: {}", scoring::score_as(&reroll, 5));
+    println!("Score as 6s: {}", scoring::score_as(&reroll, 6));
     println!("4 of a kind: {}", scoring::four_of_a_kind(&reroll));
     println!("3 of a kind: {}", scoring::three_of_a_kind(&reroll));
     println!("Full House: {}", scoring::full_house(&reroll));
