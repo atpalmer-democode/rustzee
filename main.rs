829468cb77c9de31b4f5cee47b98d5f95b008665
Andrew Palmer <atp@sdf.org>
2020-02-16 16:38:00 -0600

    Extract roll display

--- main.rs
+++ main.rs
@@ -3,6 +3,22 @@ mod roll;
 use roll::{Roll, Keep};
 mod scoring;
 
+fn display(roll: &Roll) {
+    println!("Roll: {}", roll);
+    println!("Score as 1s: {}", scoring::score_as(&roll, 1));
+    println!("Score as 2s: {}", scoring::score_as(&roll, 2));
+    println!("Score as 3s: {}", scoring::score_as(&roll, 3));
+    println!("Score as 4s: {}", scoring::score_as(&roll, 4));
+    println!("Score as 5s: {}", scoring::score_as(&roll, 5));
+    println!("Score as 6s: {}", scoring::score_as(&roll, 6));
+    println!("3 of a kind: {}", scoring::three_of_a_kind(&roll));
+    println!("4 of a kind: {}", scoring::four_of_a_kind(&roll));
+    println!("Full House: {}", scoring::full_house(&roll));
+    println!("Small Straight: {}", scoring::small_straight(&roll));
+    println!("Large Straight: {}", scoring::large_straight(&roll));
+    println!("Rustzee: {}", scoring::rustzee(&roll));
+    println!("Chance: {}", scoring::chance(&roll));
+}
 
 fn main() {
     let roll = Roll::roll();
@@ -12,18 +28,5 @@ fn main() {
     println!("Keep: {}", keep);
 
     let reroll = roll.reroll(keep);
-    println!("Reroll: {}", reroll);
-    println!("Score as 1s: {}", scoring::score_as(&reroll, 1));
-    println!("Score as 2s: {}", scoring::score_as(&reroll, 2));
-    println!("Score as 3s: {}", scoring::score_as(&reroll, 3));
-    println!("Score as 4s: {}", scoring::score_as(&reroll, 4));
-    println!("Score as 5s: {}", scoring::score_as(&reroll, 5));
-    println!("Score as 6s: {}", scoring::score_as(&reroll, 6));
-    println!("3 of a kind: {}", scoring::three_of_a_kind(&reroll));
-    println!("4 of a kind: {}", scoring::four_of_a_kind(&reroll));
-    println!("Full House: {}", scoring::full_house(&reroll));
-    println!("Small Straight: {}", scoring::small_straight(&reroll));
-    println!("Large Straight: {}", scoring::large_straight(&reroll));
-    println!("Rustzee: {}", scoring::rustzee(&reroll));
-    println!("Chance: {}", scoring::chance(&reroll));
+    display(&reroll);
 }
