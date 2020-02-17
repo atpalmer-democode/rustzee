5b15ebda90625a4d7322c432333f524202c3d154
Andrew Palmer <atp@sdf.org>
2020-02-16 23:32:13 -0600

    Introduce ScoreCard

--- main.rs
+++ main.rs
@@ -6,6 +6,75 @@ mod turn;
 use roll::Keep;
 use turn::TurnState;
 
+#[derive(Default)]
+struct ScoreCard {
+    aces: i32,
+    twos: i32,
+    threes: i32,
+    fours: i32,
+    fives: i32,
+    sixes: i32,
+    three_of_a_kind: i32,
+    four_of_a_kind: i32,
+    full_house: i32,
+    small_straight: i32,
+    large_straight: i32,
+    rustzee: i32,
+    chance: i32,
+    bonus: Vec<i32>,
+}
+
+impl ScoreCard {
+    fn new() -> ScoreCard {
+        return ScoreCard::default();
+    }
+
+    fn top_subtotal(&self) -> i32 {
+        let items = [
+            self.aces,
+            self.twos,
+            self.threes,
+            self.fours,
+            self.fives,
+            self.sixes,
+        ];
+        return items.iter().sum();
+    }
+
+    fn top_bonus(&self) -> i32 {
+        return match self.top_subtotal() >= 63 {
+            true => 35,
+            false => 0,
+        };
+    }
+
+    fn top_total(&self) -> i32 {
+        return self.top_subtotal() + self.top_bonus();
+    }
+
+    fn rustzee_bonus(&self) -> i32 {
+        return self.bonus.iter().sum();
+    }
+
+    fn bottom_total(&self) -> i32 {
+        let items = [
+            self.three_of_a_kind,
+            self.four_of_a_kind,
+            self.full_house,
+            self.small_straight,
+            self.large_straight,
+            self.rustzee,
+            self.chance,
+            self.rustzee_bonus(),
+        ];
+        return items.iter().sum();
+    }
+
+    fn total(&self) -> i32 {
+        return self.top_total() + self.bottom_total();
+    }
+}
+
 mod console {
     use text_io;
 
@@ -27,6 +96,9 @@ mod console {
 }
 
 fn main() {
+    let scorecard = ScoreCard::new();
+    println!("Score: {}", scorecard.total());
+
     let mut turn = TurnState::new();
 
     turn.roll();
