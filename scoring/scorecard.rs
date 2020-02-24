53d25c113949df3b283f4cacb90a875b3d979b7a
Andrew Palmer <atp@sdf.org>
2020-02-23 23:54:21 -0600

    Remove unneeded pub modifiers

--- scoring/scorecard.rs
+++ scoring/scorecard.rs
@@ -132,51 +132,51 @@ impl ScoreCard {
         };
     }
 
-    pub fn score_aces(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_aces(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.aces, scoring::score_as(roll, 1));
     }
 
-    pub fn score_twos(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_twos(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.twos, scoring::score_as(roll, 2));
     }
 
-    pub fn score_threes(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_threes(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.threes, scoring::score_as(roll, 3));
     }
 
-    pub fn score_fours(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_fours(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.fours, scoring::score_as(roll, 4));
     }
 
-    pub fn score_fives(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_fives(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.fives, scoring::score_as(roll, 5));
     }
 
-    pub fn score_sixes(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_sixes(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.sixes, scoring::score_as(roll, 6));
     }
 
-    pub fn score_three_of_a_kind(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_three_of_a_kind(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.three_of_a_kind, scoring::three_of_a_kind(roll));
     }
 
-    pub fn score_four_of_a_kind(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_four_of_a_kind(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.four_of_a_kind, scoring::four_of_a_kind(roll));
     }
 
-    pub fn score_full_house(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_full_house(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.full_house, scoring::full_house(roll));
     }
 
-    pub fn score_small_straight(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_small_straight(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.small_straight, scoring::small_straight(roll));
     }
 
-    pub fn score_large_straight(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_large_straight(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.large_straight, scoring::large_straight(roll));
     }
 
-    pub fn score_chance(&mut self, roll: &Roll) -> Result<i32, i32> {
+    fn score_chance(&mut self, roll: &Roll) -> Result<i32, i32> {
         return Self::do_score(&mut self.chance, scoring::chance(roll));
     }
 
